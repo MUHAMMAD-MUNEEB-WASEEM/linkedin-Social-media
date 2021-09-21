be49ae94e56df53ea44654d7ea592757f9babbcd
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-09-21 09:17:12 +0500

    completed post body

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,  { useState } from 'react'
 import './Feed.css'
 import InputOptions from './InputOptions/InputOptions'
 
@@ -8,17 +8,26 @@ import ImageIcon from '@material-ui/icons/Image';
 import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
 import EventNoteIcon from '@material-ui/icons/EventNote';
 import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
+import Post from './Post/Post';
 
 function Feed() {
+
+    const [posts, setPosts] = useState([]);
+
+    const sendPost = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <div className="feed">
+
             <div className="feed__inputContainer">
 
                 <div className="feed__input">
                     <CreateIcon/>
                     <form>
                         <input type="text"/>
-                        <button type="submit">Send</button>
+                        <button onClick={sendPost} type="submit">Send</button>
                     </form>
                 </div>
 
@@ -30,6 +39,16 @@ function Feed() {
                 </div>
             
             </div>
+            {posts.map((post)=>{
+                <Post/>
+            })}
+            <Post 
+                name="Muneeb" 
+                description="This is a test" 
+                message="This works!"
+                photourl=""
+            />
+
         </div>
     )
 }
---
+++ src/components/Feed/Post/Post.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import InputOptions from '../InputOptions/InputOptions'
+import './Post.css'
+
+//material-ui
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
+import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
+import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
+import SendOutlinedIcon from '@material-ui/icons/SendOutlined';
+import { Avatar } from '@material-ui/core'
+
+function Post({ name, description, message, photourl }) {
+    return (
+        <div className="post">
+
+            <div className="post__header">
+                
+                <Avatar src={photourl} alt="photo"/>
+                <div className="post__info">
+                    <h2>{name}</h2>
+                    <p>{description}</p>
+
+                </div>
+                
+            </div>
+
+            <div className="post__body">
+                <p>{message}</p>
+            </div>
+
+            <div className="post__button">
+                <InputOptions Icon={ThumbUpAltOutlinedIcon} title="Like" color="gray" />
+                <InputOptions Icon={ChatOutlinedIcon} title="Comment" color="gray" />
+                <InputOptions Icon={ShareOutlinedIcon} title="Share" color="gray" />
+                <InputOptions Icon={SendOutlinedIcon} title="Send" color="gray" />
+            </div>
+
+        </div>
+    )
+}
+
+export default Post
